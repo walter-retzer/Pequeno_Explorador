703239c48c945a770b49c441462124852d7e9131
WDRETZER <walter_retzer@yahoo.com.br>
2022-10-19 22:03:18 -0300

    Configuração inicial do Módulo Profile.

--- profile/src/androidTest/java/com/wdretzer/profile/ExampleInstrumentedTest.kt
+++ profile/src/androidTest/java/com/wdretzer/profile/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.wdretzer.profile", appContext.packageName)
+        assertEquals("com.wdretzer.profile.test", appContext.packageName)
     }
 }
--- profile/src/main/java/com/wdretzer/profile/ui/ProfileMainActivity.kt
+++ profile/src/main/java/com/wdretzer/profile/ui/ProfileMainActivity.kt
@@ -1,42 +1,12 @@
 package com.wdretzer.profile.ui
 
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import android.os.Bundle
 import com.wdretzer.profile.R
-import com.wdretzer.profile.databinding.ActivityProfileMainBinding
 
 class ProfileMainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityProfileMainBinding
-    private lateinit var navigationControl: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProfileMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val bundle = savedInstanceState?.getBundle(BUNDLE) ?: intent.extras
-        setupNavigationController(bundle)
+        setContentView(R.layout.activity_profile_main)
     }
-
-    private fun setupNavigationController(bundle: Bundle?) {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_profile_fragment) as NavHostFragment
-        navigationControl = navHostFragment.navController
-        navigationControl.setGraph(R.navigation.profile_navigation_graph, bundle)
-    }
-
-    companion object {
-        private const val BUNDLE = "ProfileBundle"
-
-        fun start (context: Context){
-            val intent = Intent(context, ProfileMainActivity::class.java)
-            context.startActivity(intent)
-        }
-    }
-
 }
