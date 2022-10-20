254634f505f4a9e9b56a4a70cee7d6907c073b63
WDRETZER <walter_retzer@yahoo.com.br>
2022-10-19 23:21:56 -0300

    Configuração inicial da tela de SplashScreen.

--- app/src/main/java/com/wdretzer/pequeno_explorador/MainActivity.kt
+++ app/src/main/java/com/wdretzer/pequeno_explorador/MainActivity.kt
@@ -3,6 +3,8 @@ package com.wdretzer.pequeno_explorador
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.wdretzer.pequeno_explorador.splashscreen.SplashScreenActivity
 import com.wdretzer.profile.ui.ProfileMainActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
 
-        val intent = Intent(this, ProfileMainActivity ::class.java)
+        Handler().postDelayed({
+            val intent = Intent(this, ProfileMainActivity ::class.java)
+            startActivity(intent)
+        }, 5000)
+
+
+        val intent = Intent(this, SplashScreenActivity ::class.java)
         startActivity(intent)
+
     }
 }
--- app/src/main/java/com/wdretzer/pequeno_explorador/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/wdretzer/pequeno_explorador/splashscreen/SplashScreenActivity.kt
@@ -1,13 +1,13 @@
-package com.wdretzer.profile.ui
+package com.wdretzer.pequeno_explorador.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.wdretzer.profile.R
+import com.wdretzer.pequeno_explorador.R
 
-class ProfileMainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_main)
+        setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
     }
